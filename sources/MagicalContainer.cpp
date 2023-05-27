0292f11b35de07a689206f93c8fe4d4508c41f16
AmitRazi <ekarilil@gmail.com>
2023-05-27 14:55:30 +0300

    Changed _len to unsigned long

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -5,6 +5,7 @@
 #include "MagicalContainer.hpp"
 #include <cmath>
 #include <algorithm>
+#include <vector>
 
 bool MagicalContainer::isPrime(int &num) {
     for (int i = 2; i <= sqrt(num); i++) {
@@ -24,19 +25,19 @@ void MagicalContainer::addElement(int element) {
 
 void MagicalContainer::removeElement(int num) {
     int index = findIndex(num);
-    if(index != -1) {
-        index = static_cast<unsigned long>(index);
-        removePrime(_data[index]);
-        removeSorted(_data[index]);
-        _data.erase(_data.begin() + index);
+    if (index != -1) {
+        unsigned long uIndex = static_cast<unsigned long>(index);
+        removePrime(_data.at(uIndex));
+        removeSorted(_data.at(uIndex));
+        _data.erase(_data.begin() + (index));
         _len--;
     }
 }
 
-int MagicalContainer::findIndex(int &num){
-    auto position = std::find(_data.begin(),_data.end(),num);
-    if(position != _data.end()){
-        return static_cast<int>(std::distance(_data.begin(),position));
+int MagicalContainer::findIndex(int &num) {
+    auto position = std::find(_data.begin(), _data.end(), num);
+    if (position != _data.end()) {
+        return static_cast<int>(std::distance(_data.begin(), position));
     }
 
     return -1;
@@ -62,7 +63,7 @@ void MagicalContainer::removePrime(int &num) {
 
 void MagicalContainer::removeSorted(int &num) {
     auto deletePosition = std::upper_bound(_sortedData.begin(), _sortedData.end(), num);
-    if(deletePosition!= _sortedData.end() && *deletePosition == num){
+    if (deletePosition != _sortedData.end() && *deletePosition == num) {
         _sortedData.erase(deletePosition);
     }
 
@@ -77,114 +78,125 @@ int MagicalContainer::getModifications() {
 }
 
 
-int &MagicalContainer::AscendingIterator::operator*(){
-    return *_ptr;
+int &MagicalContainer::AscendingIterator::operator*() {
+    if(_ptr != &_container._sortedData[_container._len-1]) {
+        return *_ptr;
+    }
+    return _container._sortedData[0];
+
 }
 
-bool MagicalContainer::AscendingIterator::operator==(const MagicalContainer::AscendingIterator &other) const{
+bool MagicalContainer::AscendingIterator::operator==(const MagicalContainer::AscendingIterator &other) const {
     return _ptr == other._ptr;
 }
 
-bool MagicalContainer::AscendingIterator::operator!=(const MagicalContainer::AscendingIterator &other)const{
+bool MagicalContainer::AscendingIterator::operator!=(const MagicalContainer::AscendingIterator &other) const {
     return !(_ptr == other._ptr);
 }
 
-bool MagicalContainer::AscendingIterator::operator<(const MagicalContainer::AscendingIterator &other)const{
+bool MagicalContainer::AscendingIterator::operator<(const MagicalContainer::AscendingIterator &other) const {
     return (_ptr < other._ptr);
 }
 
-bool MagicalContainer::AscendingIterator::operator>(const MagicalContainer::AscendingIterator &other)const{
+bool MagicalContainer::AscendingIterator::operator>(const MagicalContainer::AscendingIterator &other) const {
     return (_ptr > other._ptr);
 }
 
-MagicalContainer::AscendingIterator &MagicalContainer::AscendingIterator::operator++(){
+MagicalContainer::AscendingIterator &MagicalContainer::AscendingIterator::operator++() {
     _ptr++;
     return *this;
 }
 
-MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin(){
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() {
     return MagicalContainer::AscendingIterator{_container};
 }
 
-MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end(){
-    return MagicalContainer::AscendingIterator{_container,&_container._sortedData[_container._len-1]+1};
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() {
+    return MagicalContainer::AscendingIterator{_container, &_container._sortedData[_container._len - 1] + 1};
 }
 
-int &MagicalContainer::PrimeIterator::operator*(){
-    return *_ptr;
+int &MagicalContainer::PrimeIterator::operator*() {
+    if(_ptr != &_container._primeData[_container._primeData.size()-1]) {
+        return *_ptr;
+    }
+
+    return _container._sortedData[0];
 }
 
-bool MagicalContainer::PrimeIterator::operator==(const MagicalContainer::PrimeIterator &other)const{
+bool MagicalContainer::PrimeIterator::operator==(const MagicalContainer::PrimeIterator &other) const {
     return _ptr == other._ptr;
 }
 
-bool MagicalContainer::PrimeIterator::operator!=(const MagicalContainer::PrimeIterator &other)const{
+bool MagicalContainer::PrimeIterator::operator!=(const MagicalContainer::PrimeIterator &other) const {
     return !(_ptr == other._ptr);
 }
 
-bool MagicalContainer::PrimeIterator::operator<(const MagicalContainer::PrimeIterator &other)const{
+bool MagicalContainer::PrimeIterator::operator<(const MagicalContainer::PrimeIterator &other) const {
     return (_ptr < other._ptr);
 }
 
-bool MagicalContainer::PrimeIterator::operator>(const MagicalContainer::PrimeIterator &other)const{
+bool MagicalContainer::PrimeIterator::operator>(const MagicalContainer::PrimeIterator &other) const {
     return (_ptr > other._ptr);
 }
 
-MagicalContainer::PrimeIterator &MagicalContainer::PrimeIterator::operator++(){
+MagicalContainer::PrimeIterator &MagicalContainer::PrimeIterator::operator++() {
     _ptr++;
     return *this;
 }
 
-MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::begin(){
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::begin() {
     return MagicalContainer::PrimeIterator{_container};
 }
 
-MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::end(){
-    return MagicalContainer::PrimeIterator{_container,&_container._primeData[_container._primeData.size()-1]+1};
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::end() {
+    return MagicalContainer::PrimeIterator{_container, &_container._primeData[_container._primeData.size() - 1] + 1};
 }
 
-int &MagicalContainer::SideCrossIterator::operator*(){
-    return *_ptr;
+int &MagicalContainer::SideCrossIterator::operator*() {
+    if(_ptr != &_container._sortedData[_container._len/2]) {
+        return *_ptr;
+    }
+    return _container._sortedData[0];
 }
 
-bool MagicalContainer::SideCrossIterator::operator==(const MagicalContainer::SideCrossIterator &other) const{
+bool MagicalContainer::SideCrossIterator::operator==(const MagicalContainer::SideCrossIterator &other) const {
     return _ptr == other._ptr;
 }
 
-bool MagicalContainer::SideCrossIterator::operator!=(const MagicalContainer::SideCrossIterator &other)const{
+bool MagicalContainer::SideCrossIterator::operator!=(const MagicalContainer::SideCrossIterator &other) const {
     return !(_ptr == other._ptr);
 }
 
-bool MagicalContainer::SideCrossIterator::operator<(const MagicalContainer::SideCrossIterator &other)const{
+bool MagicalContainer::SideCrossIterator::operator<(const MagicalContainer::SideCrossIterator &other) const {
     return (_ptr < other._ptr);
 }
 
-bool MagicalContainer::SideCrossIterator::operator>(const MagicalContainer::SideCrossIterator &other)const{
+bool MagicalContainer::SideCrossIterator::operator>(const MagicalContainer::SideCrossIterator &other) const {
     return (_ptr > other._ptr);
 }
 
-MagicalContainer::SideCrossIterator &MagicalContainer::SideCrossIterator::operator++(){
-    if(_ptr == &_container._sortedData[_container._len/2]){
-        _ptr = &_container._sortedData[_container._len-1]+1;
-    }
-    else if(_sideFlag){
-        _ptr = &_container._sortedData[_nextElement];
+MagicalContainer::SideCrossIterator &MagicalContainer::SideCrossIterator::operator++() {
+    unsigned long nextElement = static_cast<unsigned long>(_nextElement);
+    if (_ptr == &_container._sortedData[_container._len / 2]) {
+        _ptr = &_container._sortedData[_container._len - 1] + 1;
+    } else if (_sideFlag) {
+        _ptr = &_container._sortedData[nextElement];
         _sideFlag = !_sideFlag;
-    }else{
-        _ptr = &_container._sortedData[_container._len-_nextElement-1];
+    } else {
+        _ptr = &_container._sortedData[_container._len - nextElement - 1];
         _sideFlag = !_sideFlag;
     }
 
-    if(_sideFlag){
+    if (_sideFlag) {
         _nextElement++;
     }
     return *this;
 }
 
-MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::begin(){
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::begin() {
     return MagicalContainer::SideCrossIterator{_container};
 }
 
-MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::end(){
-    return MagicalContainer::SideCrossIterator{_container,&_container._sortedData[_container._len-1]+1};
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::end() {
+    return MagicalContainer::SideCrossIterator{_container, &_container._sortedData[_container._len - 1] + 1};
 }
